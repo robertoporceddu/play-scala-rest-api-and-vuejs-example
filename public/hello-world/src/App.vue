3c7ac786a6bd11fdb78cf84046f1d4afdc4a45eb
Roberto Porceddu <robertoporceddu@gmail.com>
2020-03-03 22:31:32 +0100

    Removed submodule

---
+++ public/hello-world/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HttpGetRequest url="http://localhost:9000/v1/posts" />
+  </div>
+</template>
+
+<script>
+import HttpGetRequest from "./components/HttpGetRequest.vue";
+
+export default {
+  name: "App",
+  components: {
+    HttpGetRequest
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ public/hello-world/src/components/HttpGetRequest.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ url }}</h1>
+    <p class="http-response">{{ httpResponse }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HttpGetRequest",
+  props: {
+    url: String,
+    httpResponse: String
+  },
+  mounted() {
+    axios
+      .get(this.url)
+      .then(response => (this.httpResponse = response.data))
+      .catch(() => (this.httpResponse = "Error retrieving data..."));
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+http-response {
+  font-family: "Courier New", Courier, monospace;
+  font-size: small;
+}
+</style>
